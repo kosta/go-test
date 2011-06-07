781e1e2d096143fe8552de294ec6900a717558e1
Konstantin Welke <kosta@Konstantin-Welkes-MacBook-3.local>
2011-06-07 22:22:03 +0200

    tcp server full

--- stringtest.go
+++ stringtest.go
@@ -2,11 +2,11 @@ package main
 
 import ("fmt";)
 
-func write(bytes *[]byte) {
-  for i, _ := range(*bytes) {
-    (*bytes)[i] = 'a'
+func write(bytes []byte) {
+  for i, _ := range(bytes) {
+    bytes[i] = 'a'
   }
-  fmt.Printf("%p %s\n", &bytes, bytes)
+  fmt.Printf("w %p %s\n", &bytes, bytes)
 }
 
 func writesafe(bytes []byte) {
@@ -18,6 +18,9 @@ func main() {
   fmt.Printf("s %p %s\n", &s, s)
   
   var b []byte = ([]byte)(s)
+  
+  write(([]byte)("foo"))
+  
   fmt.Printf("b %p %s\n", &b, b)
   
   fmt.Printf("s %p %s\n", &s, s)
---
+++ tcp-server.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"net"
+	"fmt"
+  "syscall"
+	"os/signal"
+  "bufio"
+)
+
+type ConnMessage struct {
+  Conn net.Conn
+  Message string
+}
+
+func main() {
+  connections := make(map[net.Conn] bool)
+
+  newMessages := make(chan ConnMessage)
+  go func() {
+    count := 0
+    for {
+      newMessages <- ConnMessage{nil, fmt.Sprintf("ping%d\r\n", count)}
+      count++
+      err := syscall.Sleep(1e10) //10 secs
+      if err != 0 {
+        return
+      }
+    }
+  }()
+  
+	tcp, tcperr := net.ListenTCP("tcp", &net.TCPAddr{})
+	if tcperr != nil {
+		fmt.Println("Error opening TCP socket:", tcperr)
+		return
+	}
+	defer tcp.Close()
+	newConnections := make(chan *net.TCPConn)
+	fmt.Println("Listening on", tcp.Addr().Network(), tcp.Addr())
+	go func() {
+    for {
+      conn, connerr := tcp.AcceptTCP()
+      if connerr != nil {
+        fmt.Println("Error accepting TCP connection:", connerr)
+      } else {
+        newConnections <- conn
+      }
+    }
+	}()
+
+	for {
+		select {
+		case signal := <-signal.Incoming:
+			fmt.Println("got signal:", signal)
+			return
+		case conn := <-newConnections:
+			fmt.Printf("Got connection from %s %s\n",
+				conn.RemoteAddr().Network(),
+				conn.RemoteAddr())
+			conn.SetNoDelay(true)
+			conn.Write([]uint8("Hello\r\nHow are you?\r\n"))
+      connections[conn] = true
+      go func() {
+        defer conn.Close()
+        reader := bufio.NewReader(conn)
+        for {
+          line, err := reader.ReadString('\n')
+          if err != nil {
+            //todo: remove conn from connections
+            fmt.Printf("error reading %s %s: %s\n",
+              conn.RemoteAddr().Network(),
+              conn.RemoteAddr(),
+              err)
+            connections[conn] = false, false
+            return //closes conn
+          }
+          newMessages <- ConnMessage{conn, string(line)}
+        }
+      }()
+    case msg := <- newMessages:
+      for conn, _ := range(connections) {  
+        if conn != msg.Conn {
+          _, err := fmt.Fprintln(conn, msg.Message)
+          if err != nil {
+            fmt.Printf("error writing%s: %s\n",
+              conn,
+              conn.RemoteAddr(),
+              err)
+            connections[conn] = false, false
+          }
+        }
+      }
+		}
+	}
+}
